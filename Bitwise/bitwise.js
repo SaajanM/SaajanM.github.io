537cc075316f930cfede1e42e9e49a0899345b92
SaajanM <saajan.maslanka@gmail.com>
2018-09-29 11:16:57 -0700

    Bitwise

---
+++ Bitwise/bitwise.js
@@ -0,0 +1,25 @@
+(function(ext) {
+    // Cleanup function when the extension is unloaded
+    ext._shutdown = function() {};
+
+    // Status reporting code
+    // Use this to report missing hardware, plugin or unsupported browser
+    ext._getStatus = function() {
+        return {status: 2, msg: 'Ready'};
+    };
+
+    ext.bitwiseAND = function(a, b) {
+        return a & b;
+    };
+
+    // Block and block menu descriptions
+    var descriptor = {
+        blocks: [
+            // Block type, block name, function name, param1 default value, param2 default value
+            ['r', '%n & %n', 'bitwiseAND', 2, 3],
+        ]
+    };
+
+    // Register the extension
+    ScratchExtensions.register('Bitwise Operators', descriptor, ext);
+})({});
